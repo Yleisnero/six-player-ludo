8860712c188052f9b32ec23ba93b8b5296539c3a
Jonas Fischer <fischer.jonas@siemens.com>
2024-12-24 16:32:32 +0100

    Add basic dice roll logic :sparkles:

--- game.py
+++ game.py
@@ -1,14 +1,14 @@
 import random
 
 
-def throw_dice() -> int:
+def roll_dice() -> int:
     return random.randint(1, 6)
 
 
 class Piece:
     def __init__(self, index):
         self.index = index
-        self.position = 0
+        self.position = None
 
 
 class Player:
@@ -17,8 +17,42 @@ class Player:
         self.start_postion = start_position
         self.pieces = [Piece(i) for i in range(4)]
 
-    def move_piece(self, piece, steps):
-        self.pieces[piece].position += steps
+    def move_piece(self, steps):
+        for piece in self.pieces:
+            # If a piece is on the starting position, it must be moved away first
+            if self.start_postion in [piece.position for piece in self.pieces]:
+                if piece.position == self.start_postion:
+                    print(
+                        f"Player {self.color} must move piece {piece.index} away from start position"
+                    )
+                    piece.position += steps
+                    return
+
+            # If rolled a six, piece must be moved out of the starting area
+            if steps == 6 and None in [piece.position for piece in self.pieces]:
+                if piece.position is None:
+                    print(
+                        f"Player {self.color} rolls a six and moves piece {piece.index} out"
+                    )
+                    piece.position = self.start_postion
+                    return
+
+        # Find most advanced piece
+        best_piece = None
+        for piece in self.pieces:
+            if best_piece is None:
+                best_piece = piece
+            if piece.position is not None and piece.position > best_piece.position:
+                best_piece = piece
+
+        # Move most advanced piece
+        if best_piece.position is not None:
+            print(f"Player {self.color} moves {steps}")
+            best_piece.position += steps
+            return
+
+        # Can't move any piece
+        print(f"Player {self.color} can't move a piece")
 
 
 class Board:
@@ -33,25 +67,28 @@ class Board:
         return f"Players: {', '.join([player.color for player in self.players])}"
 
     def play(self):
-        throw_count = 0
+        roll_count = 0
         rounds = 1
         game_over = False
         while not game_over:
             for player in self.players:
                 print(f"{player.color} is playing")
-                dice = throw_dice()
-                throw_count += 1
-                print(f"Player {player.color} throws {dice}")
-                player.move_piece(0, dice)
-                print(
-                    f"Player {player.color} is at position {player.pieces[0].position}\n"
-                )
-
-                if player.pieces[0].position >= self.length:
-                    print(
-                        f"Player {player.color} wins after {throw_count} dice throws and {rounds} rounds!"
-                    )
-                    game_over = True
-                    break
-
+                dice = roll_dice()
+                roll_count += 1
+                print(f"Player {player.color} rolls a {dice}")
+                player.move_piece(dice)
                 rounds += 1
+
+                for piece in player.pieces:
+                    if (
+                        piece.position is not None
+                        and (piece.position - player.start_postion) > self.length
+                    ):
+                        print(f"Player {player.color} wins")
+                        game_over = True
+                        break
+
+                if game_over:
+                    print(f"Game over after {roll_count} dice rolls")
+                    print(f"Game over after {rounds} rounds")
+                    break
